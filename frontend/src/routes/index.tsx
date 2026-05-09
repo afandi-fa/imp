64b12d6977018e6a723a73f8c7e34f1e50d1d911
afandi-fa <afandiahmed24@gmail.com>
2026-05-09 10:03:01 +0700

    add link on dashboard

--- frontend/src/routes/index.tsx
+++ frontend/src/routes/index.tsx
@@ -1,9 +1,14 @@
-import { createFileRoute } from '@tanstack/react-router'
+import { createFileRoute, Link } from '@tanstack/react-router'
 
 export const Route = createFileRoute('/')({
   component: RouteComponent,
 })
 
 function RouteComponent() {
-  return <div>Hello "/"!</div>
+  return <div>Hello "/"!
+    <p>Sudah punya akun? <Link  to="/login">Register</Link></p>
+    <p>Belum punya akun? <Link to="/register">Register</Link></p>
+  </div>
+  
+
 }
